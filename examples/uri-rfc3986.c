5d5667b2b5feb6db4df57c676f5931845484b3d2
Adam Risi <ajrisi@gmail.com>
2010-09-06 22:53:37 -0400

    added capturing of URI data and reporting at the end - also, checked it for leaks, its leak free

--- examples/uri-rfc3986.c
+++ examples/uri-rfc3986.c
@@ -15,6 +15,28 @@
 
 #define MAX_INPUT 2048
 
+typedef struct uri_s uri;
+struct uri_s {
+  char *scheme;
+  char *userinfo;
+  char *host;
+  int host_is_ip;
+  int port;
+  char *path;
+  char *query;
+  char *fragment;
+};
+
+/* Private functions */
+static void set_scheme(char **uri_string, int scheme_len, void *global_context, void *local_context);
+static void set_userinfo(char **uri_string, int userinfo_len, void *global_context, void *local_context);
+static void set_host(char **uri_string, int host_len, void *global_context, void *local_context);
+static void set_host_is_ip(char **uri_string, int not_used, void *global_context, void *local_context);
+static void set_port(char **uri_string, int port_len, void *global_context, void *local_context);
+static void set_path(char **uri_string, int path_len, void *global_context, void *local_context);
+static void set_query(char **uri_string, int query_len, void *global_context, void *local_context);
+static void set_fragment(char **uri_string, int fragment_len, void *global_context, void *local_context);
+
 /*
    URI           = scheme ":" hier-part [ "?" query ] [ "#" fragment ]
 
@@ -254,11 +276,11 @@ transition path_abempty_fsm[] =
 
 transition path_fsm[] =
   {
-    {0, FSM(path_abempty_fsm),  -1, -1, ACCEPT},
-    {0, FSM(path_absolute_fsm), -1, -1, ACCEPT},
-    {0, FSM(path_noscheme_fsm), -1, -1, ACCEPT},
-    {0, FSM(path_rootless_fsm), -1, -1, ACCEPT},
-    {0, FSM(path_empty_fsm),    -1, -1, ACCEPT},
+    {0, FSM(path_abempty_fsm),  -1, -1, ACCEPT, set_path, NULL},
+    {0, FSM(path_absolute_fsm), -1, -1, ACCEPT, set_path, NULL},
+    {0, FSM(path_noscheme_fsm), -1, -1, ACCEPT, set_path, NULL},
+    {0, FSM(path_rootless_fsm), -1, -1, ACCEPT, set_path, NULL},
+    {0, FSM(path_empty_fsm),    -1, -1, ACCEPT, set_path, NULL},
     {-1}
   };
 
@@ -561,8 +583,8 @@ transition port_fsm[] =
 
 transition host_fsm[] =
     {
-      {0, FSM(ip_literal_fsm),  -1, -1, ACCEPT, NULL, NULL, "ip literal"},
-      {0, FSM(ipv4address_fsm), -1, -1, ACCEPT, NULL, NULL, "ipv4address"},      
+      {0, FSM(ip_literal_fsm),  -1, -1, ACCEPT, set_host_is_ip, NULL, "ip literal"},
+      {0, FSM(ipv4address_fsm), -1, -1, ACCEPT, set_host_is_ip, NULL, "ipv4address"},      
       {0, FSM(reg_name_fsm),    -1, -1, ACCEPT, NULL, NULL, "reg name"},      
       {-1}
     };
@@ -579,7 +601,7 @@ transition userinfo_fsm[] =
 
 transition authority_fsm_1[] =
     {
-      {0, FSM(userinfo_fsm),  1, -1, NORMAL, NULL, NULL, "userinfo"},
+      {0, FSM(userinfo_fsm),  1, -1, NORMAL, set_userinfo, NULL, "userinfo"},
       {1, EXACT_STRING("@"), -1, -1, ACCEPT, NULL, NULL, "matching a @"},
       {-1}
     };
@@ -587,14 +609,14 @@ transition authority_fsm_1[] =
 transition authority_fsm_2[] =
     {
       {0, EXACT_STRING(":"), 1, -1, NORMAL, NULL, NULL, "matching a :"},
-      {1, FSM(port_fsm),    -1, -1, ACCEPT, NULL, NULL, "port"},
+      {1, FSM(port_fsm),    -1, -1, ACCEPT, set_port, NULL, "port"},
       {-1}
     };
 
 transition authority_fsm[] =
     {
       {0, FSM(authority_fsm_1),  1, 1},
-      {1, FSM(host_fsm),         2, -1, ACCEPT, NULL, NULL, "host"},
+      {1, FSM(host_fsm),         2, -1, ACCEPT, set_host, NULL, "host"},
       {2, FSM(authority_fsm_2), -1, -1, ACCEPT},
       {-1}
     };
@@ -613,26 +635,26 @@ transition scheme_fsm[] =
 transition relative_part_fsm[] =
     {
       {0, EXACT_STRING("//"),      1, -1},
-      {0, FSM(path_absolute_fsm), -1, -1, ACCEPT},
-      {0, FSM(path_noscheme_fsm), -1, -1, ACCEPT},
-      {0, FSM(path_empty_fsm),    -1, -1, ACCEPT},
+      {0, FSM(path_absolute_fsm), -1, -1, ACCEPT, set_path, NULL},
+      {0, FSM(path_noscheme_fsm), -1, -1, ACCEPT, set_path, NULL},
+      {0, FSM(path_empty_fsm),    -1, -1, ACCEPT, set_path, NULL},
    
       {1, FSM(authority_fsm),      2, -1},
-      {2, FSM(path_abempty_fsm),  -1, -1, ACCEPT},
+      {2, FSM(path_abempty_fsm),  -1, -1, ACCEPT, set_path, NULL},
       {-1}
     };
 
 transition relative_ref_fsm_1[] =
     {
       {0, EXACT_STRING("?"), 1, -1, NORMAL, NULL, NULL, "matching ?-query"},
-      {1, FSM(query_fsm),   -1, -1, ACCEPT, NULL, NULL, "query"},
+      {1, FSM(query_fsm),   -1, -1, ACCEPT, set_query, NULL, "query"},
       {-1}
     };
 
 transition relative_ref_fsm_2[] =
     {
       {0, EXACT_STRING("#"),    1, -1, NORMAL, NULL, NULL, "matching #-fragment"},
-      {1, FSM(fragment_fsm),   -1, -1, ACCEPT, NULL, NULL, "fragment"},
+      {1, FSM(fragment_fsm),   -1, -1, ACCEPT, set_fragment, NULL, "fragment"},
       {-1}
     };
 
@@ -647,25 +669,25 @@ transition relative_ref_fsm[] =
 transition hier_part_fsm[] =
     {
       {0, EXACT_STRING("//"),      1, -1, NORMAL, NULL, NULL, "matching //"},
-      {0, FSM(path_absolute_fsm), -1, -1, ACCEPT, NULL, NULL, "path_absolute"},
-      {0, FSM(path_rootless_fsm), -1, -1, ACCEPT, NULL, NULL, "path rootless"},
-      {0, FSM(path_empty_fsm),    -1, -1, ACCEPT, NULL, NULL, "path empty"},
+      {0, FSM(path_absolute_fsm), -1, -1, ACCEPT, set_path, NULL, "path_absolute"},
+      {0, FSM(path_rootless_fsm), -1, -1, ACCEPT, set_path, NULL, "path rootless"},
+      {0, FSM(path_empty_fsm),    -1, -1, ACCEPT, set_path, NULL, "path empty"},
    
       {1, FSM(authority_fsm),      2, -1, NORMAL, NULL, NULL, "authority"},
-      {2, FSM(path_abempty_fsm),  -1, -1, ACCEPT, NULL, NULL, "path abempty"},
+      {2, FSM(path_abempty_fsm),  -1, -1, ACCEPT, set_path, NULL, "path abempty"},
       {-1}
     };
 
 transition absolute_uri_fsm_1[] =
     {
       {0, EXACT_STRING("?"), 1, -1},
-      {1, FSM(query_fsm),   -1, -1, ACCEPT},
+      {1, FSM(query_fsm),   -1, -1, ACCEPT, set_query, NULL},
       {-1}
     };
 
 transition absolute_uri_fsm[] =
     {
-      {0, FSM(scheme_fsm),          1, -1},
+      {0, FSM(scheme_fsm),          1, -1, NORMAL, set_scheme, NULL},
       {1, EXACT_STRING(":"),        2, -1, NORMAL, NULL, NULL, "matching :"},
       {2, FSM(hier_part_fsm),       3, -1, ACCEPT},
       {3, FSM(absolute_uri_fsm_1), -1, -1, ACCEPT},
@@ -675,20 +697,20 @@ transition absolute_uri_fsm[] =
 transition uri_fsm_1[] =
     {
       {0, EXACT_STRING("?"), 1, -1, NORMAL, NULL, NULL, "matching ?-query"},
-      {1, FSM(query_fsm),   -1, -1, ACCEPT, NULL, NULL, "query"},
+      {1, FSM(query_fsm),   -1, -1, ACCEPT, set_query, NULL, "query"},
       {-1}
     };
 
 transition uri_fsm_2[] =
     {
       {0, EXACT_STRING("#"),    1, -1, NORMAL, NULL, NULL, "matching #-fagment"},
-      {1, FSM(fragment_fsm),   -1, -1, ACCEPT, NULL, NULL, "fragment"},
+      {1, FSM(fragment_fsm),   -1, -1, ACCEPT, set_fragment, NULL, "fragment"},
       {-1}
     };
 
 transition uri_fsm[] =
     {
-      {0, FSM(scheme_fsm),     1, -1, NORMAL, NULL, NULL, "scheme"},
+      {0, FSM(scheme_fsm),     1, -1, NORMAL, set_scheme, NULL, "scheme"},
       {1, EXACT_STRING(":"),   2, -1, NORMAL, NULL, NULL, "matching : in uri"},
       {2, FSM(hier_part_fsm),  3, -1, ACCEPT, NULL, NULL, "hier-part"},
       {3, FSM(uri_fsm_1),      4, -1, ACCEPT},
@@ -703,11 +725,198 @@ transition uri_reference_fsm[] =
       {-1}
     };
 
+/** 
+ * Set the scheme in the global content, a uri type variable
+ * 
+ * @param uri_string the raw URI string
+ * @param scheme_len the length of the scheme string
+ * @param global_context a pointer to a URI type
+ * @param local_context should be NULL, it gets ignored
+ */
+static void set_scheme(char **uri_string, int scheme_len, void *global_context, void *local_context)
+{
+  uri *u = (uri*)global_context;
+  u->scheme = malloc(scheme_len+1);
+  if(u->scheme == NULL) {
+    /* could not allocate space to store the scheme - print an error and abort */
+    fprintf(stderr, "Could not malloc the space to store the scheme string\n");
+    exit(1);
+  }
+  memcpy(u->scheme, *uri_string, scheme_len);
+  u->scheme[scheme_len] = '\0';
+}
+
+static void set_userinfo(char **uri_string, int userinfo_len, void *global_context, void *local_context)
+{
+  uri *u = (uri*)global_context;
+  u->userinfo = malloc(userinfo_len+1);
+  if(u->userinfo == NULL) {
+    /* could not allocate space to store the userinfo - print an error and abort */
+    fprintf(stderr, "Could not malloc the space to store the userinfo string\n");
+    exit(1);
+  }
+  memcpy(u->userinfo, *uri_string, userinfo_len);
+  u->userinfo[userinfo_len] = '\0';
+}
+
+static void set_host(char **uri_string, int host_len, void *global_context, void *local_context)
+{
+  uri *u = (uri*)global_context;
+  u->host = malloc(host_len+1);
+  if(u->host == NULL) {
+    /* could not allocate space to store the host - print an error and abort */
+    fprintf(stderr, "Could not malloc the space to store the host string\n");
+    exit(1);
+  }
+  memcpy(u->host, *uri_string, host_len);
+  u->host[host_len] = '\0';
+}
+
+static void set_host_is_ip(char **uri_string, int not_used, void *global_context, void *local_context)
+{
+  uri *u = (uri*)global_context;
+  u->host_is_ip = 1;
+}
+
+static void set_port(char **uri_string, int port_len, void *global_context, void *local_context)
+{
+  uri *u = (uri*)global_context;
+  char *port_tmp;
+  port_tmp = malloc(port_len+1);
+  if(port_tmp == NULL) {
+    /* could not allocate space to store the port - print an error and abort */
+    fprintf(stderr, "Could not malloc the space to store the port string\n");
+    exit(1);
+  }
+  memcpy(port_tmp, *uri_string, port_len);
+  port_tmp[port_len] = '\0';
+  u->port = atoi(port_tmp);
+  free(port_tmp);
+}
+
+static void set_path(char **uri_string, int path_len, void *global_context, void *local_context)
+{
+  uri *u = (uri*)global_context;
+  u->path = malloc(path_len+1);
+  if(u->path == NULL) {
+    /* could not allocate space to store the path - print an error and abort */
+    fprintf(stderr, "Could not malloc the space to store the path string\n");
+    exit(1);
+  }
+  memcpy(u->path, *uri_string, path_len);
+  u->path[path_len] = '\0';
+}
+
+static void set_query(char **uri_string, int query_len, void *global_context, void *local_context)
+{
+  uri *u = (uri*)global_context;
+  u->query = malloc(query_len+1);
+  if(u->query == NULL) {
+    /* could not allocate space to store the query - print an error and abort */
+    fprintf(stderr, "Could not malloc the space to store the query string\n");
+    exit(1);
+  }
+  memcpy(u->query, *uri_string, query_len);
+  u->query[query_len] = '\0';
+}
+
+static void set_fragment(char **uri_string, int fragment_len, void *global_context, void *local_context)
+{
+  uri *u = (uri*)global_context;
+  u->fragment = malloc(fragment_len+1);
+  if(u->fragment == NULL) {
+    /* could not allocate space to store the fragment - print an error and abort */
+    fprintf(stderr, "Could not malloc the space to store the fragment string\n");
+    exit(1);
+  }
+  memcpy(u->fragment, *uri_string, fragment_len);
+  u->fragment[fragment_len] = '\0';
+}
+
+static char *xstrdup(char *in) {
+  char *out;
+  size_t len;
+  if(in == NULL) {
+    return NULL;
+  }
+  len = strlen(in);
+  
+  out = malloc(len+1);
+  if(out == NULL) {
+    return NULL;
+  }
+  memcpy(out, in, len);
+  out[len] = '\0';
+
+  return out;
+}
+
+void* duplicate_uri(void *uri_in)
+{
+  uri *old_uri = (uri*)uri_in;
+  uri *new_uri;
+
+  if(old_uri == NULL) {
+    return NULL;
+  }
+
+  new_uri = malloc(sizeof(uri));
+  if(new_uri == NULL) {
+    return NULL;
+  }
+
+  new_uri->host_is_ip = old_uri->host_is_ip;
+  new_uri->port = old_uri->port;
+  new_uri->scheme = xstrdup(old_uri->scheme);
+  new_uri->userinfo = xstrdup(old_uri->userinfo);
+  new_uri->host = xstrdup(old_uri->host);
+  new_uri->path = xstrdup(old_uri->path);
+  new_uri->query = xstrdup(old_uri->query);
+  new_uri->fragment = xstrdup(old_uri->fragment);
+
+  return new_uri;
+}
+
+void free_uri(void *uri_in)
+{
+  uri *to_free = (uri*)uri_in;
+  if(to_free == NULL) {
+    return;
+  }
+
+  free(to_free->scheme);
+  free(to_free->userinfo);
+  free(to_free->host);
+  free(to_free->path);
+  free(to_free->query);
+  free(to_free->fragment);
+  free(to_free);
+}
+
 int main(int argc, char **argv)
 {
   char *str, *ostr;
   int ret;
+  uri *parsed_uri;
  
+  uri *d;
+
+  /* initialize the URI structure - this needs pointers set to NULL to
+     be correct! */
+  parsed_uri = malloc(sizeof(uri));
+  if(parsed_uri == NULL) {
+    fprintf(stderr, "Unable to allocate space on the heap for a URI\n");
+  }
+
+  parsed_uri->host_is_ip = 0;
+  parsed_uri->port = -1;
+  parsed_uri->scheme = NULL;
+  parsed_uri->userinfo = NULL;
+  parsed_uri->host = NULL;
+  parsed_uri->path = NULL;
+  parsed_uri->query = NULL;
+  parsed_uri->fragment = NULL;
+
   /* read a string from the user */
   str = ostr = calloc(MAX_INPUT+1, 1);
   if(str == NULL) {
@@ -718,15 +927,24 @@ int main(int argc, char **argv)
   fgets(str, MAX_INPUT, stdin);
 
   printf("Processing %d byte string...\n", (int)strlen(str));
-  /* process string through FSM */
-  ret = run_fsm(uri_reference_fsm, &str, NULL);
+
+  ret = run_fsm(uri_reference_fsm, &str, (void**)&parsed_uri, duplicate_uri, free_uri);
   if(ret < 0) {
     printf("Unable to execute FSM on string: %s\n", str);
-    return EXIT_FAILURE;
+  } else {  
+    printf("URI scheme: %s\n", parsed_uri->scheme);
+    printf("URI userinfo: %s\n", parsed_uri->userinfo);
+    printf("URI host: %s\n", parsed_uri->host);
+    printf("  Is host an IP address?: %s\n", (parsed_uri->host_is_ip) ? "yes" : "no");
+    printf("URI port: %d\n", parsed_uri->port);
+    printf("URI path: %s\n", parsed_uri->path);
+    printf("URI query: %s\n", parsed_uri->query);
+    printf("URI fragment: %s\n", parsed_uri->fragment); 
+    printf("\nFSM Done - processed %d characters: \"%.*s\".\n", ret, ret, ostr);
   }
-  
-  printf("\nFSM Done - processed %d characters: \"%.*s\".\n", ret, ret, ostr);
  
+  free_uri(parsed_uri);
+  free(ostr);
   return 0;
 }
 
